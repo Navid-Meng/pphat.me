e38ed33a45de8ea6e4030a177b83e0ba813253ed
pphatdev <pphat.kits@gmail.com>
2026-03-13 10:40:45 +0700

    fix: update structured data type and properties in AboutStructuredData component

--- src/components/about-structured-data.tsx
+++ src/components/about-structured-data.tsx
@@ -13,9 +13,16 @@ import {
 export default function AboutStructuredData() {
     const structuredData = {
         "@context": "https://schema.org",
-        "@type": "AboutPage",
+        "@type": "ProfilePage",
+        "@id": `${NEXT_PUBLIC_APP_URL}/about`,
+        "name": `About ${appName}`,
+        "description": `I'm ${PERSON_NAME} (${PERSON_ALTERNATE_NAME}), a ${PERSON_JOB_TITLE}.`,
+        "url": `${NEXT_PUBLIC_APP_URL}/about`,
+        "dateCreated": "2021-01-01",
+        "dateModified": new Date().toISOString().split('T')[0],
         "mainEntity": {
             "@type": "Person",
+            "@id": `${NEXT_PUBLIC_APP_URL}#person`,
             "name": PERSON_NAME,
             "alternateName": PERSON_ALTERNATE_NAME,
             "description": `I'm ${PERSON_NAME} (${PERSON_ALTERNATE_NAME}), a ${PERSON_JOB_TITLE}.`,
@@ -29,10 +36,7 @@ export default function AboutStructuredData() {
                 GITHUB_URL,
                 LINKEDIN_URL
             ]
-        },
-        "name": `About ${appName}`,
-        "description": `I'm ${PERSON_NAME} (${PERSON_ALTERNATE_NAME}), a ${PERSON_JOB_TITLE}.`,
-        "url": `${NEXT_PUBLIC_APP_URL}/about`
+        }
     };
 
     return (
